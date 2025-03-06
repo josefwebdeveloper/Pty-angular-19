171dd7878b5b7638783017fe0c0f88394f659564
Josef Shainskiy <josef2011@gmail.com>
2025-03-06 23:58:03 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DoublePlayComponent } from './components/animations/double-play/double-play.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DoublePlayComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
