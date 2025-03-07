1760e024decf279ecaf5c509d7d5d0120a8e6869
Josef Shainskiy <josef2011@gmail.com>
2025-03-07 15:28:04 +0200

    continue with animation

--- src/app/shared/animations/money-fly-animation/money-fly-animation.component.ts
+++ src/app/shared/animations/money-fly-animation/money-fly-animation.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-money-fly-animation',
+  standalone: true,
   imports: [],
   templateUrl: './money-fly-animation.component.html',
   styleUrl: './money-fly-animation.component.scss'
 })
-export class MoneyFlyAnimationComponent {
-
+export class MoneyFlyAnimationComponent implements OnInit {
+  // Animation will start automatically when component is initialized
+  ngOnInit() {
+    // Animation is controlled via CSS
+  }
 }
