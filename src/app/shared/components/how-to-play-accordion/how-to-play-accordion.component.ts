0592cb0e0be7a985f53864404259d7428fc17e66
Josef Shainskiy <josef2011@gmail.com>
2025-03-08 21:07:22 +0200

    add accordion

--- src/app/shared/components/how-to-play-accordion/how-to-play-accordion.component.ts
+++ src/app/shared/components/how-to-play-accordion/how-to-play-accordion.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-how-to-play-accordion',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './how-to-play-accordion.component.html',
   styleUrl: './how-to-play-accordion.component.scss'
 })
 export class HowToPlayAccordionComponent {
-
+  isExpanded: boolean = false;
+  
+  toggleAccordion(): void {
+    this.isExpanded = !this.isExpanded;
+  }
 }
